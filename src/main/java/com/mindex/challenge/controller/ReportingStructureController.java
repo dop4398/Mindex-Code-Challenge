7645c0da76d13e23c0b9dc6d5d64943bb92a5e04
David <dop4398@g.rit.edu>
2022-11-30 22:35:59 -0500

    reporting structure partially working

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -2,7 +2,6 @@ package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/mindex/challenge/service/ReportingStructureService.java
+++ src/main/java/com/mindex/challenge/service/ReportingStructureService.java
@@ -1,7 +1,6 @@
 package com.mindex.challenge.service;
 
 import com.mindex.challenge.data.ReportingStructure;
-import com.mindex.challenge.dao.EmployeeRepository;
 
 public interface ReportingStructureService {
     ReportingStructure read(String id);
