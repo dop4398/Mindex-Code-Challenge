481efdc327de82319dcfb9d935d797f465ea9857
David <dop4398@g.rit.edu>
2022-12-02 11:02:12 -0500

    created new compensation repo w/ employee ID keys

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -5,6 +5,7 @@ import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.service.CompensationService;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.dao.CompensationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +17,14 @@ public class CompensationServiceImpl  implements CompensationService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private CompensationRepository compensationRepository;
 
     @Override
     public Compensation create(Compensation compensation) {
         LOG.debug("Creating compensation [{}]", compensation);
 
-        // compensation.setEmployee(null);
-        // compensation.setSalary(0);
-        // compensation.setEffectiveDate(null);
-
-        // add compensation to some list?
+        compensationRepository.insert(compensation);
 
         return compensation;
     }
@@ -34,14 +33,16 @@ public class CompensationServiceImpl  implements CompensationService {
     public Compensation read(String id) {
         LOG.debug("Retrieving compensation for employee with id [{}]", id);
 
-        Compensation compensation = new Compensation();
         Employee employee = employeeRepository.findByEmployeeId(id);
         if (employee == null) {
             throw new RuntimeException("Invalid employeeId: " + id);
         }
-        compensation.setEmployee(employee);
+        
+        Compensation compensation = compensationRepository.findByEmployeeEmployeeId(id);
+        if (compensation == null) {
+            throw new RuntimeException("Missing compensation for employee with id " + id);
+        }
     
-        // persistance...? read from a persistant list?
         return compensation;
     }
 }
